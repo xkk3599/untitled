20d33bbeab6cca11934057574fab948d48011899
Administrator <xkk3599@163.com>
2020-01-06 12:41:07 +0800

    1111

---
+++ src/test.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String []arg)
+    {
+        Scanner scanner =new Scanner(System.in);
+        int age =scanner.nextInt();
+        System.out.println("111");
+        System.out.println(age);
+    }
+}
